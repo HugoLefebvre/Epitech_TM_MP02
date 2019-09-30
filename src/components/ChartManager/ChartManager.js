62841c914c5753e6450da06d0d4c15289992b1ec
Steven <steven.nnguyen@epitech.eu>
2019-09-30 07:36:09 -0700

    [Chart Manager] setLineChart improved

--- src/components/ChartManager/ChartManager.js
+++ src/components/ChartManager/ChartManager.js
@@ -31,7 +31,7 @@ export default {
       startTimeBar: '', 
       endTimeBar: '', 
       barData : [], 
-      xkeyBar : 'lineDay', 
+      xkeyBar : 'barWeek', 
       ykeysBar :[], 
       labelsBar :[], 
       barColors :[],
@@ -52,9 +52,10 @@ export default {
     self.user_ID = localStorage.IdUser;
 
     self.setLineChart(); 
+    self.setBarChart(); 
     
   },
-  methods: {
+  methods:{
 
     pad: function(number){
       var str = number;
@@ -64,12 +65,63 @@ export default {
       return str;
     },
 
-    getWeekNumber: function(d) {
+    getWeekNumber: function(d) {   //d has to be Date format
       d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
       d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
       var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
       var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
       return weekNo;
+    },
+
+    setBarChart: function(){
+      var self = this; 
+
+      self.endTimeBar = new Date; 
+      self.startTimeBar = new Date(new Date().getFullYear(), 0, 1);
+
+      var Stime = self.startTimeBar.getFullYear() + '-' + self.pad(self.startTimeBar.getMonth()+1) + '-' + 
+      self.pad(self.startTimeBar.getDate()) + '+' + '00:00:00'
+
+      var Etime = self.endTimeBar.getFullYear() + '-' + self.pad(self.endTimeBar.getMonth()+1) + '-' + 
+      self.pad(self.endTimeBar.getDate()) + '+' + self.pad(self.endTimeBar.getHours()) + ':' + 
+      self.pad(self.endTimeBar.getMinutes()) + ':' + self.pad(self.endTimeBar.getSeconds());
+
+      console.log(Stime); 
+      console.log(Etime);
+
+      axios({
+        method: 'get',
+        url: 'http://localhost:4000/api/workingtimes/'+ self.user_ID + '?start=' + Stime + '&end=' + Etime,
+        headers: {
+          'Authorization': 'Bearer ' + localStorage.AccessKey
+        }
+      })
+      .then(function (response) {
+        var data = response.data.data;
+        var tot;
+        var obj;
+
+        var i = 0;
+        for (i = 0; i < data.length; i++) {
+          if(i>0 && self.getWeekNumber(new Date(data[i].start)) == self.getWeekNumber(new Date(data[i-1].start))){
+            tot += Math.abs(new Date(data[i].end) - new Date(data[i].start))/(1000 * 60 * 60);
+            self.lineData.pop(); 
+          } 
+          else{
+            tot = Math.abs(new Date(data[i].end) - new Date(data[i].start))/(1000 * 60 * 60);
+          }
+          obj = { barWeek: 'Week ' + self.getWeekNumber(new Date(data[i].start)), a: tot};
+          self.lineData.push(obj); 
+        }
+        self.ykeys.push('a');
+        self.labels.push('Working Time');
+        self.lineColors.push('#FF6384');
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+      
+
     },
 
     setLineChart: function(){
@@ -93,32 +145,22 @@ export default {
       }
     })
     .then(function (response) {
-      
-      var data = response.data.data; 
+      var data = response.data.data;
       var tot;
       var obj;
 
       //Setting line chart data 
       var i = 0;
       for (i = 0; i < data.length; i++) {
-        if (i>0){
-          if (data[i].start.substring(0,10) == data[i-1].start.substring(0,10)){
-            tot += Math.abs(new Date(data[i].end) - new Date(data[i].start))/(1000 * 60 * 60);
-            self.lineData.pop(); 
-            obj = {lineDay: data[i].start.substring(0,10).toString(), a: tot};
-            self.lineData.push(obj); 
-          }
-          else{
-            tot = Math.abs(new Date(data[i].end) - new Date(data[i].start))/(1000 * 60 * 60);
-            obj = {lineDay: data[i].start.substring(0,10).toString(), a: tot};
-            self.lineData.push(obj); 
-          }
+        if (i>0 && data[i].start.substring(0,10) == data[i-1].start.substring(0,10)){
+          tot += Math.abs(new Date(data[i].end) - new Date(data[i].start))/(1000 * 60 * 60);
+          self.lineData.pop(); 
         }
         else{
           tot = Math.abs(new Date(data[i].end) - new Date(data[i].start))/(1000 * 60 * 60);
-            obj = {lineDay: data[i].start.substring(0,10).toString(), a: tot};
-            self.lineData.push(obj); 
         }
+        obj = {lineDay: data[i].start.substring(0,10).toString(), a: tot};
+        self.lineData.push(obj); 
       }
       self.ykeys.push('a');
       self.labels.push('Working Time');
